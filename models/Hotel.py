4bc2269c600e12d3524e8427ad4a1d2045dd195e
macbf <macbf0@gmail.com>
2021-10-29 22:48:39 -0300

    Incremento da classe HotelModel Adicionado estilo das colunas no banco de dados

--- models/Hotel.py
+++ models/Hotel.py
@@ -1,4 +1,15 @@
-class HotelModel:
+from sql_alchemy import banco
+
+class HotelModel(banco.Model):
+    __tablename__ = 'hoteis'
+
+    hotel_id = banco.Column(banco.String, primary_key=True)
+    nome = banco.Column(banco.String(80))
+    estrelas = banco.Column(banco.Float(precision=2))
+    preco = banco.Column(banco.Float(precision=2))
+    cidade = banco.Column(banco.String(50))
+
+
     def __init__(self, hotel_id, nome, estrelas, preco, cidade):
         self.hotel_id = hotel_id
         self.nome = nome
